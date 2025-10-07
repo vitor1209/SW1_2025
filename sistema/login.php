eca6657c607445eed185a1f9dd19c1c8240690da
Vitor Lopes <137239415+vitor1209@users.noreply.github.com>
2025-10-07 09:00:26 -0300

    Add files via upload

---
+++ sistema/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="processa_login.php" method="POST">
+       email: <input type="email" name="email" id=""><br>
+       senha: <input type="text" name="senha" id=""><br>
+       <input type="submit" value="LOGAR">
+    </form>
+</body>
+</html>
---
+++ sistema/processa_login.php
@@ -0,0 +1,34 @@
+<?php
+
+    session_start();
+    include_once 'conexao.php';
+
+    $email= $_POST['email'];
+    $senha= $_POST['senha'];
+
+    $consulta = "SELECT * FROM usuarios WHERE email = :email AND senha = :senha ";
+
+    $stmt = $pdo->prepare($consulta);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':senha', $senha);
+
+    $stmt->execute();
+
+    $num_registros = $stmt->rowCount();
+
+    $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($resultado);
+
+    if ($num_registros == 0) {
+        // echo "PODE NAO";
+        header('Location: index.php');
+
+    } else {
+        $_SESSION['id'] = $resultado['id'];
+        $_SESSION['nome'] = $resultado['nome'];
+        $_SESSION['email'] = $resultado['email'];
+
+        header('Location: restrita.php');
+    }
+?>
